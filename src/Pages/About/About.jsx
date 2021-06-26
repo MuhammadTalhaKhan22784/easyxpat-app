1c3959ac3f9402dc59aa1b4764c436957be4acb7
MuhammadTalhaKhan22784 <talhatariq0608@gmail.com>
2021-06-26 04:04:06 -0700

    first commit 14

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,9 +1,15 @@
+import { Box } from "@material-ui/core";
 import React from "react";
 import Header from "../../Layout/Header";
+import AboutUsComp from "../../Pages/About/Component/AboutUsComp";
+import "./About.css"
 const About = () => {
   return (
     <>
       <Header />
+      <Box className="container">
+        <AboutUsComp />
+      </Box>
     </>
   );
 };
