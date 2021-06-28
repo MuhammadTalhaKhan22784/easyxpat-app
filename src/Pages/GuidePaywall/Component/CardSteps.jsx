6fea4bcf75ee11d8a9d7bcceb1ef4398a8b48e83
MuhammadTalhaKhan22784 <talhatariq0608@gmail.com>
2021-06-28 05:27:15 -0700

    done 3

--- src/Pages/GuidePaywall/Component/CardSteps.jsx
+++ src/Pages/GuidePaywall/Component/CardSteps.jsx
@@ -3,24 +3,14 @@ import { Box } from "@material-ui/core";
 import data from "../steps.json";
 
 const CardSteps = () => {
-  // console.log(data);
-  // let [show, setShow] = useState({
-  //   f: false,
-  //   f1: false,
-  //   f2: false,
-  //   f3: false,
-  //   f4: false,
-  //   f5: false,
-  // });
-  const [show,setShow] = useState('')
+  const [show, setShow] = useState("");
 
   const handleOpen = (e) => {
-    // setShow({ ...show, [e]: !show[e] });
-    setShow(e)
+    setShow(e);
   };
-  const handleClose =(e)=>{
-    setShow(e)
-  }
+  const handleClose = (e) => {
+    setShow(e);
+  };
   return (
     <React.Fragment>
       <Box className="card_steps_section">
@@ -29,10 +19,14 @@ const CardSteps = () => {
           {data.map((val) => (
             <Box className="card_steps_content" key={val.id}>
               <Box
-                className={`c_steps ${show !==val.id ? "c_steps2" : "c_steps1"}`}
-                onClick={() =>{show!==val.id? handleOpen(val.id):handleClose('')}}
+                className={`c_steps ${
+                  show !== val.id ? "c_steps2" : "c_steps1"
+                }`}
+                onClick={() => {
+                  show !== val.id ? handleOpen(val.id) : handleClose("");
+                }}
               >
-                <h2>{show!==val.id? val.name : val.description}</h2>
+                <h2>{show !== val.id ? val.name : val.description}</h2>
               </Box>
               <p style={{ display: show !== val.id ? "none" : "block" }}>
                 {val.description}
@@ -48,116 +42,6 @@ const CardSteps = () => {
               </p>
             </Box>
           ))}
-          {/*               
-          <Box className="card_steps_content">
-            <Box
-              className={`c_steps ${!show.f1 ? "c_steps2" : "c_steps1"}`}
-              onClick={() => handleOpen("f1")}
-            >
-              <h2>{!show.f1 ? "STEP 1" : "Prepare your university degree"}</h2>
-            </Box>
-            <p style={{ display: !show.f1 ? "none" : "block" }}>
-              German university degree, a foreign university degree which is
-              accepted or a foreign university degree that is similar to a
-              German one.
-              <br />
-              <br />
-              The central department for the foreign education service (ZAB)
-              offers an online- database where one can check which foreign
-              university degrees are accepted in Germany. If the database does
-              not offer helpful information, EU blue card applicants need to
-              apply for an individual check of their degrees. This check is not
-              for free and needs to be presented at the foreign department.
-            </p>
-          </Box>
-
-          <Box className="card_steps_content">
-            <Box
-              className={`c_steps ${!show.f2 ? "c_steps2" : "c_steps1"}`}
-              onClick={() => handleOpen("f2")}
-            >
-              <h2>{!show.f2 ? "STEP 1" : "Prepare your university degree"}</h2>
-            </Box>
-            <p style={{ display: !show.f2 ? "none" : "block" }}>
-              German university degree, a foreign university degree which is
-              accepted or a foreign university degree that is similar to a
-              German one.
-              <br />
-              <br />
-              The central department for the foreign education service (ZAB)
-              offers an online- database where one can check which foreign
-              university degrees are accepted in Germany. If the database does
-              not offer helpful information, EU blue card applicants need to
-              apply for an individual check of their degrees. This check is not
-              for free and needs to be presented at the foreign department.
-            </p>
-          </Box>
-
-          <Box className="card_steps_content">
-            <Box
-              className={`c_steps ${!show.f3 ? "c_steps2" : "c_steps1"}`}
-              onClick={() => handleOpen("f3")}
-            >
-              <h2>{!show.f3 ? "STEP 1" : "Prepare your university degree"}</h2>
-            </Box>
-            <p style={{ display: !show.f3 ? "none" : "block" }}>
-              German university degree, a foreign university degree which is
-              accepted or a foreign university degree that is similar to a
-              German one.
-              <br />
-              <br />
-              The central department for the foreign education service (ZAB)
-              offers an online- database where one can check which foreign
-              university degrees are accepted in Germany. If the database does
-              not offer helpful information, EU blue card applicants need to
-              apply for an individual check of their degrees. This check is not
-              for free and needs to be presented at the foreign department.
-            </p>
-          </Box>
-
-          <Box className="card_steps_content">
-            <Box
-              className={`c_steps ${!show.f4 ? "c_steps2" : "c_steps1"}`}
-              onClick={() => handleOpen("f4")}
-            >
-              <h2>{!show.f4 ? "STEP 1" : "Prepare your university degree"}</h2>
-            </Box>
-            <p style={{ display: !show.f4 ? "none" : "block" }}>
-              German university degree, a foreign university degree which is
-              accepted or a foreign university degree that is similar to a
-              German one.
-              <br />
-              <br />
-              The central department for the foreign education service (ZAB)
-              offers an online- database where one can check which foreign
-              university degrees are accepted in Germany. If the database does
-              not offer helpful information, EU blue card applicants need to
-              apply for an individual check of their degrees. This check is not
-              for free and needs to be presented at the foreign department.
-            </p>
-          </Box>
-
-          <Box className="card_steps_content">
-            <Box
-              className={`c_steps ${!show.f5 ? "c_steps2" : "c_steps1"}`}
-              onClick={() => handleOpen("f5")}
-            >
-              <h2>{!show.f5 ? "STEP 1" : "Prepare your university degree"}</h2>
-            </Box>
-            <p style={{ display: !show.f5 ? "none" : "block" }}>
-              German university degree, a foreign university degree which is
-              accepted or a foreign university degree that is similar to a
-              German one.
-              <br />
-              <br />
-              The central department for the foreign education service (ZAB)
-              offers an online- database where one can check which foreign
-              university degrees are accepted in Germany. If the database does
-              not offer helpful information, EU blue card applicants need to
-              apply for an individual check of their degrees. This check is not
-              for free and needs to be presented at the foreign department.
-            </p>
-          </Box> */}
         </Box>
       </Box>
     </React.Fragment>
